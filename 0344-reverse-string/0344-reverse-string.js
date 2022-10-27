9f0d70505389130e01a6bcb44b3dfa820f4ea792
Bryan Valdovinos <bvaldovinos24@gmail.com>
2022-10-27 15:12:28 -0700

    Time: 162 ms (46.86%), Space: 49.8 MB (21.72%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -3,5 +3,14 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    s.reverse();
+    let left = 0, right = s.length - 1;
+
+  while (left < right) {
+    const temp = s[left];
+    s[left] = s[right];
+    s[right] = temp;
+
+    left++;
+    right--;
+  }
 };
